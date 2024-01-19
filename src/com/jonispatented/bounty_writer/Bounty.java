ce3a7e36dd1bb9a35fde50147b1d0f2c0d17f233
JonIsPatented <rockerjon01@gmail.com>
2024-01-18 20:16:05 -0500

    Added automatic generation of bucket_check.mcfunction

--- src/com/jonispatented/bounty_writer/Bounty.java
+++ src/com/jonispatented/bounty_writer/Bounty.java
@@ -3,11 +3,13 @@ package com.jonispatented.bounty_writer;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 public record Bounty(String itemID, long itemValue, Position chestPosition) {
 
     public void generateBountyFile(int bountyNumber) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new PrintWriter("bounty_output/bounty_" + bountyNumber));
+        BufferedWriter writer = new BufferedWriter(new PrintWriter("bounty_output/bounty_"
+                + bountyNumber + ".mcfunction"));
         writer.write("execute as @e[tag=Overseer,limit=1] store result score @s " +
                         "item_count run data get block -396 76 -65 " +
                         "Items[{id:\"" + itemID + "\"}].Count\n" +
@@ -30,6 +32,17 @@ public record Bounty(String itemID, long itemValue, Position chestPosition) {
                         "loot insert " + chestPosition.x() + ' ' + chestPosition.y() + ' ' + chestPosition.z() +
                         " mine -396 76 -65 minecraft:air{drop_contents:1b}\n" +
                         "data remove block -396 76 -65 Items[{id:\"" + itemID + "\"}]");
+        writer.close();
+    }
+
+    public static void generateBucketCheck(List<Bounty> bounties) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new PrintWriter("bounty_output/bucket_check.mcfunction"));
+
+        for (int i = 0; i < bounties.size(); i++)
+            writer.write("execute if data block -396 76 -65 {Items:[{id:\"" +
+                    bounties.get(i).itemID() + "\"}]} run function communism:bounty_" + (i + 1) + '\n');
+
+        writer.write("\ndata remove block -396 76 -65 Items");
         writer.close();
     }
 
